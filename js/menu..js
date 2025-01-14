a0b74d62f369b8cdd95f100a3a7190a7543cff4a
[Marcel Prats Ustrell] <marcelprats@uoc.edu>
2025-01-14 04:12:48 +0100

    Optimización de scripts y HTML para mejorar el rendimiento web 1.3

--- js/menu..js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    function toggleMenu() {
-        const menu = document.querySelector('.navbar ul');
-        menu.classList.toggle('active');
-    }
-
-    document.querySelector('.hamburger-menu').addEventListener('click', toggleMenu);
-});
